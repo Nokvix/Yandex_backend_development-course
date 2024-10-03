64df8ab774ce923acad22c28781c65f46009c4a3
Nokvix <igor.gass2003@mail.ru>
2024-10-03 22:24:57 +0500

    Сделал задание из темы "Знакомство с ООП" урок "Инкапсуляция", но не проходит тест на платформе

---
+++ Задания с лекций/Знакомство_с_ООП_Инкапсуляция.py
@@ -0,0 +1,54 @@
+class Employee:
+    vacation_days: int = 28
+
+    def __init__(self, first_name: str, second_name: str, gender: str) -> None:
+        self.first_name: str = first_name
+        self.second_name: str = second_name
+        self.gender: str = gender
+        self.remaining_vacation_days: int = Employee.vacation_days
+        self._employee_id: int = self.__generate_employee_id()
+
+    def __generate_employee_id(self) -> int:
+        return hash(self.first_name + self.second_name + self.gender)
+
+    def consume_vacation(self, days) -> None:
+        self.remaining_vacation_days -= days
+
+    def get_vacation_details(self) -> str:
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
+
+class FullTimeEmployee(Employee):
+    def __init__(self, first_name: str, second_name: str,
+                 gender: str, salary: int) -> None:
+        super().__init__(first_name, second_name, gender)
+        self.__salary = salary
+
+    def __get_vacation_salary(self) -> float:
+        return self.__salary * 0.8
+
+    def get_unpaid_vacation(self, start_date: str, number_days: int) -> str:
+        return (
+            f'Начало неоплачиваемого отпуска: {start_date}, '
+            f'продолжительность: {number_days} дней.')
+
+
+class PartTimeEmployee(Employee):
+    vacation_days: int = 14
+
+    def __init__(self, first_name: str, second_name: str, gender: str):
+        super().__init__(first_name, second_name, gender)
+        self.remaining_vacation_days: int = self.vacation_days
+
+
+employee = Employee('Иванка', 'Иванова', 'ж')
+print(employee._employee_id)
+
+full_time_employee = FullTimeEmployee('Иван', 'Иванов', 'м', 50000)
+print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+print(full_time_employee._employee_id)
+
+part_time_employee = PartTimeEmployee('Анна', 'Петрова', 'ж')
+part_time_employee.consume_vacation(5)
+print(part_time_employee.get_vacation_details())
+print(part_time_employee._employee_id)
