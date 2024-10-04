6186b1f3abf3cb36e80589f2eda12eeeee141db8
Nokvix <igor.gass2003@mail.ru>
2024-10-04 15:15:14 +0500

    Toic "Документирование кода"

--- Projects/tic_tac_toe/gameparts/parts.py
+++ Projects/tic_tac_toe/gameparts/parts.py
@@ -1,11 +1,24 @@
 class Board:
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+    """Класс, который описывает игровое поле."""
 
-    def make_move(self, row: int, col: int, player: str):
+    field_size = 3
+
+    def __init__(self):
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(
+                self.field_size)
+        ]
+
+    def make_move(self, row, col, player):
         self.board[row][col] = player
 
     def display(self):
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
