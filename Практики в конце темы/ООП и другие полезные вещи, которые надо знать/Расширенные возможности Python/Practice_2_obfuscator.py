8f761d7dd11ff023556f6353aa27b1718d288a60
Nokvix <igor.gass2003@mail.ru>
2024-10-10 17:15:13 +0500

    Do practice 2 of the topic “Расширенные возможности Python”

---
+++ Практики в конце темы/ООП и другие полезные вещи, которые надо знать/Расширенные возможности Python/Practice_2_obfuscator.py
@@ -0,0 +1,25 @@
+def obfuscator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        name = result['name']
+        password = result['password']
+
+        result = {
+            'name': name[0] + len(name[1:-1]) * '*' + name[-1],
+            'password': len(password) * '*'
+        }
+
+        return result
+
+    return wrapper
+
+
+@obfuscator
+def get_credentials():
+    return {
+        'name': 'StasBasov',
+        'password': 'iamthebest'
+    }
+
+
+print(get_credentials())
