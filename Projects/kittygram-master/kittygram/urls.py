6c3599f0960a29a4ffcd731257a12aca2bcb060c
Nokvix <igor.gass2003@mail.ru>
2024-11-20 16:25:56 +0500

    Переделал на viewset

--- Projects/kittygram-master/kittygram/urls.py
+++ Projects/kittygram-master/kittygram/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 from cats import views
 
+
+router = SimpleRouter()
+
+router.register('cats', views.CatViewSet)
+
+# urlpatterns = [
+#    path('cats/', views.CatList.as_view()),
+#    path('cats/<int:pk>/', views.CatDetail.as_view()),
+# ]
+
 urlpatterns = [
-   path('cats/', views.CatList.as_view()),
-   path('cats/<int:pk>/', views.CatDetail.as_view()),
+   path('', include(router.urls))
 ]
 
 
