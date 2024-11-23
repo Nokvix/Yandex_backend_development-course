1c095ee46c186488a32631bebc776cdb98e89818
Nokvix <igor.gass2003@mail.ru>
2024-11-23 16:45:38 +0500

    Создал новый ViewSet, унаследовавшись от собственных базовых. Зарегистрировал ViewSet

--- Projects/kittygram_plus-master/cats/views.py
+++ Projects/kittygram_plus-master/cats/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import mixins
 from .models import Cat, Owner
 from .serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
@@ -25,3 +26,18 @@ class CatViewSet(viewsets.ModelViewSet):
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
+
+
+class UpdateDeleteViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                          viewsets.GenericViewSet):
+    pass
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
