76780ecaca5cc710aebecfcdfc4e4fa3912f2250
Nokvix <igor.gass2003@mail.ru>
2024-11-23 18:36:13 +0500

    Добавил авторизацию по jwt токену

--- Projects/kittygram_plus-master/kittygram_plus/urls.py
+++ Projects/kittygram_plus-master/kittygram_plus/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken import views
 from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
@@ -12,4 +12,6 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
