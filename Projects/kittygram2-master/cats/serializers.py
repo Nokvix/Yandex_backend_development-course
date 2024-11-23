6d79699f279631d2969b34f6aaefd811976b93bf
Nokvix <igor.gass2003@mail.ru>
2024-11-23 20:16:06 +0500

    Добавил валидацию на уровне поля birth_year

--- Projects/kittygram2-master/cats/serializers.py
+++ Projects/kittygram2-master/cats/serializers.py
@@ -27,7 +27,7 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
     age = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
@@ -50,3 +50,9 @@ class CatSerializer(serializers.ModelSerializer):
                 AchievementCat.objects.create(
                     achievement=current_achievement, cat=cat)
             return cat
+
+    def validate_birth_year(self, value):
+        year = dt.datetime.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError('Проверьте год рождения!')
+        return value
