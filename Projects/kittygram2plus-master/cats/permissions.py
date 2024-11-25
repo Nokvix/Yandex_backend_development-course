131256899a85ea9c5c73ed0b645ede92b6f9d039
Nokvix <igor.gass2003@mail.ru>
2024-11-25 12:52:33 +0500

    Небольшой рефакторинг

--- Projects/kittygram2plus-master/cats/permissions.py
+++ Projects/kittygram2plus-master/cats/permissions.py
@@ -11,7 +11,7 @@ class OwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
+
 class ReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
-
