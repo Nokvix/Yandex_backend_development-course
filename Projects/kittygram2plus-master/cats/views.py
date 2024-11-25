c93ff853c53a79b2f0a99bd1d2092575bc6a9c8e
Nokvix <igor.gass2003@mail.ru>
2024-11-25 15:01:52 +0500

    Добавил поиск по полю

--- Projects/kittygram2plus-master/cats/views.py
+++ Projects/kittygram2plus-master/cats/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.throttling import ScopedRateThrottle
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .throttling import WorkingHoursRateThrottle
 from .models import Achievement, Cat, User
@@ -14,9 +15,10 @@ class CatViewSet(viewsets.ModelViewSet):
     permission_classes = (OwnerOrReadOnly,)
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
     throttle_scope = 'low_request'
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     pagination_class = CatsPaginator
     filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
